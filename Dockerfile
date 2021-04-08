bef5c863db20493b0dc00f0accf18314a979c684
Robbert <rhessing@descartes.com>
2021-04-08 15:52:40 +0200

    Included nomad environment variables

--- Dockerfile
+++ Dockerfile
@@ -16,16 +16,14 @@ RUN apk --update add \
         && unzip consul-template.zip \
         && mv consul-template /usr/local/bin/consul-template \
         && chmod 755 /usr/local/bin/consul-template \
-        && mkdir -p /etc/consul-template/config.d /etc/consul-template/template.d \
+        && mkdir -p /etc/haproxy/static /etc/consul-template/config.d /etc/consul-template/template.d \
         && apk del wget zip \
         && rm -rf /consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip \
         && rm -rf /tmp/* \
         && rm /var/cache/apk/* \ 
         && rm -rf /var/lib/apt/lists/* \ 
 
-COPY consul.hcl /etc/consul-template/config.d/consul.hcl
 COPY haproxy.hcl /etc/consul-template/config.d/haproxy.hcl
-COPY haproxy.ctmpl /etc/consul-template/template.d/haproxy.ctmpl
 COPY haproxy-reload.sh /usr/local/bin/
 COPY docker-entrypoint.sh /usr/local/bin/
 RUN chmod 755 /usr/local/bin/docker-entrypoint.sh /usr/local/bin/haproxy-reload.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,83 @@
 #!/bin/sh
 set -e
 
+cat << EOF >> /etc/consul-template/config.d/consul.hcl
+consul {
+    address = "${CONSUL_CLIENT_HTTPS}"
+    retry {
+        enabled = true
+        attempts = 12
+        backoff = "250ms"
+    }
+}
+
+reload_signal = "SIGHUP"
+kill_signal = "SIGINT"
+max_stale = "10m"
+log_level = "warn"
+pid_file = "/run/consul-template.pid"
+
+wait {
+    min = "5s"
+    max = "10s"
+}
+
+EOF
+
+cat << EOF >> /etc/haproxy/static/default.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Default LB</title>
+</head>
+<body>
+
+<h1>${NOMAD_GROUP_NAME}</h1>
+
+</body>
+</html> 
+EOF
+
+cat << EOF >> /etc/consul-template/template.d/haproxy.ctmpl
+global
+    log /dev/log local0
+    log /dev/log local1 notice
+    chroot /var/lib/haproxy
+    stats socket /var/run/haproxy_admin.sock mode 660 level admin
+    pidfile /var/run/haproxy.pid
+    user  haproxy
+    group haproxy
+    daemon
+
+defaults
+    log    global
+    mode   http
+    option httplog
+    option dontlognull
+    timeout connect 50000
+    timeout client  50000
+    timeout server  50000
+
+resolvers consul
+    nameserver consul ${COSUL_CLIENT_DNS}
+    accepted_payload_size 8192
+    hold valid 3s
+
+frontend http_front
+    bind *:80
+    use_backend %[req.hdr(host),lower,word(1,:)]
+    default_backend haproxy
+
+backend haproxy
+    mode http
+    http-request set-log-level silent
+    errorfile 503 /etc/haproxy/static/default.html
+
+{{ range $i, $service := services }}{{ range $tag := .Tags }}{{ if $tag | regexMatch "^domain=.+" }}{{ $domain := index (. | split "=") 1 }}{{ if $service.Tags | contains "edge" }}
+backend {{$domain}}
+  server-template {{$service.Name}} 10 _{{$service.Name}}._tcp.service.consul resolvers consul resolve-opts allow-dup-ip resolve-prefer ipv4 check
+{{end}}{{end}}{{end}}{{end}}
+EOF
+
 echo "Starting Consul template"
 /usr/local/bin/consul-template -config /etc/consul-template/config.d
