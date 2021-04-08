4b0b7fcc3c5e0bd2c0bf3caae61c4f2deefdc0ad
Robbert <rhessing@descartes.com>
2021-04-08 18:53:04 +0200

    fix for variable usage in sed containing slashes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-sed -i "s/%%CONSUL_CLIENT_HTTPS%%/${CONSUL_CLIENT_HTTPS}/g" /etc/consul-template/config.d/consul.hcl
-sed -i "s/%%NOMAD_GROUP_NAME%%/${NOMAD_GROUP_NAME}/g" /etc/haproxy/static/default.html
-sed -i "s/%%CONSUL_CLIENT_DNS%%/${CONSUL_CLIENT_DNS}/g" /etc/consul-template/template.d/haproxy.ctmpl
+sed -i "s~%%CONSUL_CLIENT_HTTPS%%~${CONSUL_CLIENT_HTTPS}~g" /etc/consul-template/config.d/consul.hcl
+sed -i "s~%%NOMAD_GROUP_NAME%%~${NOMAD_GROUP_NAME}~g" /etc/haproxy/static/default.html
+sed -i "s~%%CONSUL_CLIENT_DNS%%~${CONSUL_CLIENT_DNS}~g" /etc/consul-template/template.d/haproxy.ctmpl
 
 
 echo "Starting Consul template"
